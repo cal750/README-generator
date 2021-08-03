d3e68ce56c33f1e8dd470c291ca99c902e4388b6
Seth Nightingale <sethnightingale10@gmail.com>
2021-08-03 16:42:58 +0800

    Adding Javascript to create a skeleton of my README

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// the start of index.js
+//adding my constants that allow communication to other files in the repository
+const inquirer = require('inquirer');
+const fs = require('fs')
+const markdown = require('markdown');
+const {createMarkdown} = require('./markdown/createMarkdown');
+
+const writeFileAsync = markdown.promisify(fs.writeFile)
--- markdown/markdown.js
+++ markdown/markdown.js
@@ -1,1 +1,41 @@
-// the start of markdown
+function createMarkdown(answers) {
+    //function to generate answers for my README
+    return  `
+    ## Title
+    ${answers.Title}
+  
+    ## Description 
+    ${answers.Description}
+    
+    ## Table of Contents
+    1. [Installation](#Installation)
+    2. [Usage](#Usage)
+    3. [License](#License)
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests)
+    6. [Questions](#Questions)
+  
+    ## Installation
+    ${answers.Installation}
+  
+    ## Usage
+    ${answers.Usage}
+  
+    ## License
+    ${answers.License}
+  
+    ## Contributing
+    ${answers.Contributing}
+  
+    ## Tests
+    ${answers.Tests}
+  
+    ## Questions
+    ${answers.Questions}
+    ${answers.LinkedIn}
+    ${answers.Email}
+    
+  `;
+  }
+  
+  module.exports = {createMarkdown};
